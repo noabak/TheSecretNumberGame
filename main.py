485f7f83cc449b66e088e2a415885e9d16d10356
noabak <naouar.bakkar@gmail.com>
2019-10-13 23:07:14 +0200

    intial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+secret = 3
+guess= int(input("Please enter a number:"))
+
+if guess==secret:
+  print("**** CONGRATULATIONS! *** You did it!")
+else:
+  print("Oups, the number you entered is wrong!")
+
+
